d736889d156a5c70e4232dde0110fda8d2767cc8
Dmitry <d.k.kulikov@gmail.com>
2018-07-22 02:40:14 +0300

    Trying out husky with prettier
    - Lecture #

---
+++ src/containers/Checkout/Checkout.js
@@ -0,0 +1,26 @@
+import React,
+
+{     Component      } from 'react';
+
+import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary'
+
+class Checkout extends Component {
+    render(    ) {
+        return (
+            <div>
+                
+            </div    >
+        );
+    }
+}
+
+
+
+
+
+export default Checkout;
+
+
+
+
+
