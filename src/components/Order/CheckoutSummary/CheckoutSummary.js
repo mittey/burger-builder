d09e6aba90f8f03118aec7ae750aad6fc047be4f
Dmitry <d.k.kulikov@gmail.com>
2018-07-22 02:51:29 +0300

    Building the checkout container
    - Lecture #207

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -3,13 +3,13 @@ import React from "react";
 import Burger from "../../Burger/Burger";
 import Button from "../../UI/Button/Button";
 
-import classes from "./checkoutSummary.css";
+import classes from "./CheckoutSummary.css";
 
-const checkoutSummary = () => {
+const checkoutSummary = props => {
   return (
     <div className={classes.CheckoutSummary}>
       <h1>We hope it tastes well!</h1>
-      <div style={{ width: "300px", height: "300px", margin: "auto" }}>
+      <div style={{ width: "100%", margin: "auto" }}>
         <Burger ingredients={props.ingredients} />
       </div>
       <Button btnType="Danger">CANCEL</Button>
--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -3,8 +3,21 @@ import React, { Component } from "react";
 import CheckoutSummary from "../../components/Order/CheckoutSummary/CheckoutSummary";
 
 class Checkout extends Component {
+  state = {
+    ingredients: {
+      salad: 2,
+      meat: 2,
+      bacon: 2,
+      cheese: 2
+    }
+  };
+
   render() {
-    return <div />;
+    return (
+      <div>
+        <CheckoutSummary ingredients={this.state.ingredients} />
+      </div>
+    );
   }
 }
 
