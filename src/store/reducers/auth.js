4874534d732fc10ca9d68c4e7d12097183320dec
Dmitry <d.k.kulikov@gmail.com>
2018-07-31 13:21:34 +0300

    Storing the Token
    - Lecture #314

---
+++ src/store/reducers/auth.js
@@ -0,0 +1,37 @@
+import * as actionTypes from "../actions/actionTypes";
+import produce from "immer";
+
+const initialState = {
+  token: null,
+  userId: null,
+  error: null,
+  loading: false
+};
+
+const reducer = (state = initialState, action) =>
+  produce(state, draft => {
+    switch (action.type) {
+      case actionTypes.AUTH_START:
+        draft.error = null;
+        draft.loading = true;
+        break;
+
+      case actionTypes.AUTH_SUCCESS:
+        draft.token = action.idToken;
+
+        draft.loading = false;
+        draft.error = null;
+        draft.userId = action.userId;
+        break;
+
+      case actionTypes.AUTH_FAIL:
+        draft.error = action.error;
+        draft.loading = false;
+        break;
+
+      default:
+        break;
+    }
+  });
+
+export default reducer;
