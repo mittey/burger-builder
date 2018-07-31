a11ab8d4ede8c1ec6a4d607d4899ef7f5c3475c0
Dmitry <d.k.kulikov@gmail.com>
2018-07-31 08:39:06 +0300

    Adding an Auth Form
    - Lecture #310

---
+++ src/containers/Auth/Auth.js
@@ -0,0 +1,120 @@
+import React, { Component } from "react";
+import produce from "immer";
+
+import Input from "../../components/UI/Input/Input";
+import Button from "../../components/UI/Button/Button";
+
+import classes from "./Auth.css";
+
+class Auth extends Component {
+  state = {
+    controls: {
+      email: {
+        elementType: "input",
+        elementConfig: {
+          type: "email",
+          placeholder: "Mail Address"
+        },
+        value: "",
+        validation: {
+          required: true,
+          isEmail: true
+        },
+        valid: false,
+        touched: false
+      },
+      password: {
+        elementType: "input",
+        elementConfig: {
+          type: "password",
+          placeholder: "Password"
+        },
+        value: "",
+        validation: {
+          required: true,
+          minLength: 6
+        },
+        valid: false,
+        touched: false
+      }
+    }
+  };
+
+  checkValidity(value, rules) {
+    let isValid = true;
+    if (!rules) {
+      return true;
+    }
+
+    if (rules.required) {
+      isValid = value.trim() !== "" && isValid;
+    }
+
+    if (rules.minLength) {
+      isValid = value.length >= rules.minLength && isValid;
+    }
+
+    if (rules.maxLength) {
+      isValid = value.length <= rules.maxLength && isValid;
+    }
+
+    if (rules.isEmail) {
+      const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+      isValid = pattern.test(value) && isValid;
+    }
+
+    if (rules.isNumeric) {
+      const pattern = /^\d+$/;
+      isValid = pattern.test(value) && isValid;
+    }
+
+    return isValid;
+  }
+
+  inputChangedHandler = (event, controlName) => {
+    const value = event.target.value;
+
+    this.setState(
+      produce(draft => {
+        draft.controls[controlName].value = value;
+        draft.controls[controlName].valid = this.checkValidity(
+          value,
+          this.state.controls[controlName].validation
+        );
+        draft.controls[controlName].touched = true;
+      })
+    );
+  };
+
+  render = () => {
+    const formElementsArray = [];
+
+    for (let key in this.state.controls) {
+      formElementsArray.push({ id: key, config: this.state.controls[key] });
+    }
+
+    const form = formElementsArray.map(formElement => (
+      <Input
+        key={formElement.id}
+        elementType={formElement.config.elementType}
+        elementConfig={formElement.config.elementConfig}
+        value={formElement.config.value}
+        changed={event => this.inputChangedHandler(event, formElement.id)}
+        invalid={!formElement.config.valid}
+        shouldValidate={formElement.config.validation}
+        touched={formElement.config.touched}
+      />
+    ));
+
+    return (
+      <div className={classes.Auth}>
+        <form>
+          {form}
+          <Button btnType="Success">SUBMIT</Button>
+        </form>
+      </div>
+    );
+  };
+}
+
+export default Auth;
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -116,8 +116,11 @@ class ContactData extends Component {
     this.props.onOrderBurger(order);
   };
 
-  checkValidity = (value, rules) => {
+  checkValidity(value, rules) {
     let isValid = true;
+    if (!rules) {
+      return true;
+    }
 
     if (rules.required) {
       isValid = value.trim() !== "" && isValid;
@@ -131,8 +134,18 @@ class ContactData extends Component {
       isValid = value.length <= rules.maxLength && isValid;
     }
 
+    if (rules.isEmail) {
+      const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+      isValid = pattern.test(value) && isValid;
+    }
+
+    if (rules.isNumeric) {
+      const pattern = /^\d+$/;
+      isValid = pattern.test(value) && isValid;
+    }
+
     return isValid;
-  };
+  }
 
   inputChangedHandler = (event, inputIdentifier) => {
     const updateOrderForm = { ...this.state.orderForm };
