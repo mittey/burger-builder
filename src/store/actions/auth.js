793e8a9403fc42aa7d490ab59bfea0ac1578c9d8
Dmitry <d.k.kulikov@gmail.com>
2018-08-13 15:44:39 +0300

    Fixing Connect + Routing Errors
    - Lecture #323

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -90,14 +90,16 @@ export const authCheckState = () => dispatch => {
     dispatch(logout());
   } else {
     const expirationDate = new Date(localStorage.getItem("expirationDate"));
-    if (expirationDate > new Date()) {
+    if (expirationDate <= new Date()) {
+      dispatch(logout());
+    } else {
       const userId = localStorage.getItem("userId");
       dispatch(authSuccess(token, userId));
       dispatch(
-        checkAuthTimeout(expirationDate.getSeconds() - new Date().getSeconds())
+        checkAuthTimeout(
+          (expirationDate.getTime() - new Date().getTime()) / 1000
+        )
       );
-    } else {
-      dispatch(logout());
     }
   }
 };
