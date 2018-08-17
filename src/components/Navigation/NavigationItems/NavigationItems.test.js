b31c0af24af8457351640c165266e049dbadfa7d
Dmitry <d.k.kulikov@gmail.com>
2018-08-17 11:00:24 +0300

    Revert "Add failing test for investigation"
    This reverts commit 54f3913a62e2dc0b1155670af6c07ea682153915.

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -21,6 +21,6 @@ describe("<NavigationItems />", () => {
 
   it("should render three <NavigationItem /> elements if authenticated", () => {
     wrapper.setProps({ isAuthenticated: true });
-    expect(wrapper.find(NavigationItem)).toHaveLength(4);
+    expect(wrapper.find(NavigationItem)).toHaveLength(3);
   });
 });
